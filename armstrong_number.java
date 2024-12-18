cc24f259ae970278858b1bf5fbf80ccab9c95575
Harshini038 <141545251+Harshini038@users.noreply.github.com>
2024-12-18 13:49:08 +0530

    Create armstrong_number.java

---
+++ armstrong_number.java
@@ -0,0 +1,21 @@
+
+public static void main(String[] args)
+{
+Scanner sc = new Scanner(System.in
+);
+int n=sc.nextInt();
+int d sc.nextInt();
+int temp=n;
+int res=0;
+int r=0;
+while(n>0){ r= n%10;
+}
+res+ Math.pow(r,d); n=n/10;
+if(res ==temp) {
+System.out.println("Armstrong"
+);
+}
+else{
+}
+}
+System.out.println("no");
