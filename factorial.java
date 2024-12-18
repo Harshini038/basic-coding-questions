96d57499730269912d21e737275c67a9f208f74d
Harshini038 <141545251+Harshini038@users.noreply.github.com>
2024-12-18 13:56:08 +0530

    Create factorial.java

---
+++ factorial.java
@@ -0,0 +1,15 @@
+
+
+Scanner sc = new Scanner(System.in
+);
+int a sc.nextInt();
+System.out.println(factorial(a));
+}
+public static long factorial(int n){
+if(n==1){
+}
+return 1;
+else{
+return (n*factorial(n-1));
+}
+}
